059d9996014ca30a186bf46150f12365a85753ea
Deepanshu Rajput <deepanshu.dlri@gmail.com>
2023-10-03 22:53:51 +0530

    Data Ingestion completed

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("/config/workspace/config/config.yaml")
+PARAMS_FILE_PATH = Path("/config/workspace/params.yaml")
+print(CONFIG_FILE_PATH)
+print(PARAMS_FILE_PATH)
